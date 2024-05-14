7c97bd9a40cec9989bf56cc12a3c0cc93ede3691
pab-h <dev.pab.2020@gmail.com>
2024-05-14 16:14:48 -0300

    feat: repositório dos posts com o método de criação

---
+++ postm/repositories/posts.py
@@ -0,0 +1,23 @@
+from postm.entities.post import Post
+from postm.database import database 
+from uuid import uuid4 as uuid
+from datetime import datetime
+
+class PostRepository(object):
+
+    def __init__(self) -> None:
+        self.collection = database["posts"]
+
+    def createPost(self, title: str, description: str, image: str = None) -> Post:
+        post = Post(
+            id = uuid(),
+            title = title,
+            description = description,
+            image = image,
+            created_at = datetime.now(),
+            updated_at = datetime.now()
+        )
+
+        self.collection.insert_one(
+            document = post.toJson
+        )
