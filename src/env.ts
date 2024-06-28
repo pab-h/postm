55114ce9f341fbde1d8746d92db63c9fcc275753
pab-h <dev.pab.2020@gmail.com>
2024-06-28 11:10:58 -0300

    feat: UPLOAD_PATH env

--- src/env.ts
+++ src/env.ts
@@ -4,7 +4,8 @@ const envSchema = z.object({
     SERVER_HOST: z.string().url(),
     SERVER_PORT: z.coerce.number(),
     JWT_KEY: z.string(),
-    SALT_ROUNDS: z.coerce.number()
+    SALT_ROUNDS: z.coerce.number(),
+    UPLOAD_PATH: z.string()
 });
 
 const env = envSchema.parse(process.env);
