0c2480c8906ce30e6d16ec8d27a7987b7bbbd652
pab-h <dev.pab.2020@gmail.com>
2024-06-28 11:09:43 -0300

    feat: post model

---
+++ prisma/migrations/20240628114726_post/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "post" (
+    "id" TEXT NOT NULL,
+    "image" TEXT,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "post_pkey" PRIMARY KEY ("id")
+);
